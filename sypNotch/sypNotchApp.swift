8bb2fbfb715edbf24c4dc1e8afad76688f2e0dbf
Paula Sanz <paula@sanz.eu>
2025-08-31 18:17:00 +0300

    Initial Commit

---
+++ sypNotch/sypNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  sypNotchApp.swift
+//  sypNotch
+//
+//  Created by Paula on 31/8/25.
+//
+
+import SwiftUI
+
+@main
+struct sypNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
